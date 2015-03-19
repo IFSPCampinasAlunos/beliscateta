e941dde166ec9b956e6608c7fb72c4180c4cf2e1
Daniel Teixeira <daniel.t.dt@gmail.com>
2015-03-19 18:30:33 -0300

    Alterando local da churrasqueira

--- app/main/main-controller.js
+++ app/main/main-controller.js
@@ -7,7 +7,7 @@
     function MainController() {
       this.when = new Date('March, 22 2015, 10:00 AM');
       this.where = {
-        place: 'Clube da Bosch, Churrasqueira #4',
+        place: 'Clube da Bosch, Churrasqueira #2',
         address: 'Av. Robert Bosch, 168-480, Parque Via Norte, Campinas - São Paulo, Brasil'
       };
       this.howMuch = 'R$ 35,00 + Bebida (pagar até terça, 17/03)';
